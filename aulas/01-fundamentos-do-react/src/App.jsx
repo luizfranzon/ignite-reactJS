8edd2a7c0ab975e610b28c2dd08f9647d85596b6
Luiz <luizfranzon05@hotmail.com>
2022-07-04 09:57:40 -0300

    :art: Criado Component Post

--- aulas/01-fundamentos-do-react/src/App.jsx
+++ aulas/01-fundamentos-do-react/src/App.jsx
@@ -1,6 +1,17 @@
+import { Post } from "./Post"
+
 export function App() {
 
     return (
-        <h1>Hello Ignite!</h1>
+        <div>
+            <Post
+                author="Luiz"
+                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+            <Post
+                author="Carlos"
+                content="Melhor post do mundo "
+            />
+        </div>
     )
 }
