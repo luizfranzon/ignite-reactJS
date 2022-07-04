882c627051f492735f5311c463a8f408d1b0fc66
Luiz <luizfranzon05@hotmail.com>
2022-07-04 14:11:48 -0300

    :lipstick: Sidebar component CSS finished

--- aulas/01-fundamentos-do-react/src/App.jsx
+++ aulas/01-fundamentos-do-react/src/App.jsx
@@ -1,21 +1,29 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header/Header"
 import { Post } from "./Post"
 
+import styles from "./App.module.css"
+
 import "./global.css"
+import { Sidebar } from "./components/Sidebar/Sidebar"
 
 export function App() {
 
     return (
         <div>
             <Header />
-            <Post
-                author="Luiz"
-                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            />
-            <Post
-                author="Carlos"
-                content="Melhor post do mundo "
-            />
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    <Post
+                        author="Luiz"
+                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                    />
+                    <Post
+                        author="Carlos"
+                        content="Melhor post do mundo "
+                    />
+                </main>
+            </div>
         </div>
     )
 }
