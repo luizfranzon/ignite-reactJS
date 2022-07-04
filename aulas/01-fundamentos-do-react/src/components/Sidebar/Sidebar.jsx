4c066b3bb22fee4f3861f00f86220ffa620cde18
Luiz <luizfranzon05@hotmail.com>
2022-07-04 14:11:28 -0300

    :truck: Reorganizing component folder

---
+++ aulas/01-fundamentos-do-react/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import styles from "./Sidebar.module.css"
+
+import {PencilSimpleLine} from "phosphor-react"
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1607799279861-4dd421887fb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+            />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/luizfranzon.png" />
+
+                <strong>Luiz Franzon</strong>
+                <span>Web Developer</span>
+
+            </div>
+
+            <footer>
+
+                <a href="#">
+                    <PencilSimpleLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+
+        </aside>
+    )
+}
