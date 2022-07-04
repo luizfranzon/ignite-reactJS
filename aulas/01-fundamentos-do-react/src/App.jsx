dbdfeebdbbd67e8f1888da535fd07c2a45df877d
Luiz <luizfranzon05@hotmail.com>
2022-07-04 10:13:39 -0300

    :art: Header with CSS Modules

--- aulas/01-fundamentos-do-react/src/App.jsx
+++ aulas/01-fundamentos-do-react/src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 export function App() {
 
     return (
         <div>
+            <Header />
             <Post
                 author="Luiz"
                 content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
