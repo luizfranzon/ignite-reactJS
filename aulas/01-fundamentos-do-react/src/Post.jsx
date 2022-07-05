b7517b650a9de1a0c0eec52a74fe41af65880a0e
Luiz <luizfranzon05@hotmail.com>
2022-07-05 13:59:22 -0300

    🔥Deleted old Post component

--- aulas/01-fundamentos-do-react/src/Post.jsx
+++
@@ -1,9 +0,0 @@
-export function Post(props) {
-    return(
-        <div>
-            <strong>{props.author}</strong>
-            <br />
-            <span>{props.content}</span>
-        </div>
-    )
-}
