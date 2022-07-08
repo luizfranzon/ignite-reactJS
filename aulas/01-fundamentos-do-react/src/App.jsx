abc9a2d0d188b0a8925f68458ff8e20c70827ca0
Luiz <luizfranzon05@hotmail.com>
2022-07-08 17:59:14 -0300

    🚧 Created posts array

--- aulas/01-fundamentos-do-react/src/App.jsx
+++ aulas/01-fundamentos-do-react/src/App.jsx
@@ -6,6 +6,37 @@ import styles from "./App.module.css"
 
 import "./global.css"
 
+const posts = [
+    {
+        id: 1,
+        author: {
+            avatarUrl: "https://github.com/ignys.png",
+            name: "Danolo",
+            role: "Web Developer"
+        },
+        content: [
+            { type: 'paragraph', content: "Fala galeraa 👋"},
+            { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+            { type: 'link', content: "jane.design/doctorcare"}
+        ],
+        publishedAt: new Date('2022-05-03 20:14:41')
+    },
+    {
+        id: 2,
+        author: {
+            avatarUrl: "https://github.com/diego3g.png",
+            name: "Diego Fernandes",
+            role: "CTO @Rocketseat"
+        },
+        content: [
+            { type: 'paragraph', content: "Fala galeraa 👋"},
+            { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+            { type: 'link', content: "jane.design/doctorcare"}
+        ],
+        publishedAt: new Date('2022-05-10 22:32:14')
+    },
+]
+
 export function App() {
 
     return (
