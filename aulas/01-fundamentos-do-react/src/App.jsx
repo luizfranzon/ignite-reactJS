224bf937b408a85295366d28a3494b0d080aca52
Luiz <luizfranzon05@hotmail.com>
2022-07-08 17:58:17 -0300

    :art: Added props to Post component

--- aulas/01-fundamentos-do-react/src/App.jsx
+++ aulas/01-fundamentos-do-react/src/App.jsx
@@ -14,14 +14,13 @@ export function App() {
             <div className={styles.wrapper}>
                 <Sidebar />
                 <main>
-                    <Post
-                        author="Luiz"
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    />
-                    <Post
-                        author="Carlos"
-                        content="Melhor post do mundo "
-                    />
+                    {posts.map(post => {
+                        return (<Post 
+                            author={post.author}
+                            content={post.content}
+                            publishedAt={post.publishedAt}
+                        />)
+                    })}
                 </main>
             </div>
         </div>
