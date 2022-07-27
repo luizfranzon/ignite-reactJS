920930445d72c74c982694cbc0cff955677e227c
Luiz Franzon <luizfranzon05@hotmail.com>
2022-07-27 14:33:06 -0300

    🚧 Main files added

---
+++ desafios/react-toDo/src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header"
+
+import { PlusCircle } from "phosphor-react"
+import styles from "./App.module.css"
+
+export function App() {
+    return (
+        <div>
+            <Header />
+            <div className={styles.inputBox}>
+                <input className={styles.input} type="text" placeholder="Adicione uma nova tarefa" />
+                <button className={styles.button} >Criar <PlusCircle size={20} color="#ffffff" /> </button>
+            </div>
+        </div>
+    );
+}
