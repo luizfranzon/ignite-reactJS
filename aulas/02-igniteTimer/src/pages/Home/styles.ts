89578ef0d16ccd700322218f43301dac6cc76d3f
Luiz <luizfranzon05@hotmail.com>
2022-10-17 22:27:33 -0300

    💄aligned text to center

--- aulas/02-igniteTimer/src/pages/Home/styles.ts
+++ aulas/02-igniteTimer/src/pages/Home/styles.ts
@@ -81,6 +81,7 @@ export const TaskInput = styled(BaseInput)`
 
 export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
+  text-align: center;
 `
 
 export const StartCountdownButton = styled.button`
