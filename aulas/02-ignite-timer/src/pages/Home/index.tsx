67435dbb1e6de117859718ad80bc41685ffa29d2
Luiz Franzon <luizfranzon05@hotmail.com>
2023-02-02 21:31:20 -0300

    Páginas e rotas

---
+++ aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,58 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            placeholder="Dê um nome para o seu projeto"
+            id="task"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            placeholder="00"
+            id="minutesAmount"
+            step={5}
+            max={90}
+            min={5}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play /> Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
