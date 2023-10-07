91006508cdc87b8620b58229948b1e707c63e7d9
Luiz Franzon <luizfranzon05@hotmail.com>
2023-10-06 21:36:05 -0300

    ⚡️ add: closures to setLikeCount

--- aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -13,7 +13,9 @@ export function Comment({ content, onDeleteComment, commentId }) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1)
+    setLikeCount((state) => {
+      return state + 1
+    })
   }
 
   return (
